8e9c52248aa3530e941b35e48c49e91e0426aaa4
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2024-09-26 05:30:09 -0400

    Implement game manager

--- src/game_manager.rs
+++ src/game_manager.rs
@@ -7,39 +7,114 @@ const PIECE_BLACK: char = 'B';
 const PIECE_EMPTY: char = '.';
 
 const BOARD_LABEL: [char; BOARD_DIMENSION] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const DELTAS: [[i8; 2]; 8] = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+];
 
-pub struct Move {
-    pub m_row: usize,
-    pub m_col: usize,
+type GameBoardArray = [[char; BOARD_DIMENSION]; BOARD_DIMENSION];
+
+pub struct Position {
+    pub m_row: i8,
+    pub m_col: i8,
 }
 
-pub struct GameManager {
-    m_board: [[char; BOARD_DIMENSION]; BOARD_DIMENSION],
-    m_turn: bool,
-    m_num_white: usize,
-    m_num_black: usize,
-    m_next_available_moves: Vec<Move>,
+impl Position {
+    pub fn new(row: i8, col: i8) -> Position {
+        Position {
+            m_row: row,
+            m_col: col,
+        }
+    }
+
+    pub fn is_in_bound(&self) -> bool {
+        self.m_row >= 0
+            && self.m_row < BOARD_DIMENSION as i8
+            && self.m_col >= 0
+            && self.m_col < BOARD_DIMENSION as i8
+    }
+
+    pub fn out_of_bounds(&self) -> bool {
+        false == self.is_in_bound()
+    }
 }
 
-impl GameManager {
-    pub fn new() -> GameManager {
+struct GameBoard {
+    m_board: GameBoardArray,
+}
+
+impl GameBoard {
+    fn new() -> GameBoard {
         let mut board = [[PIECE_EMPTY; BOARD_DIMENSION]; BOARD_DIMENSION];
         board[3][3] = PIECE_WHITE;
         board[4][4] = PIECE_WHITE;
         board[4][3] = PIECE_BLACK;
         board[3][4] = PIECE_BLACK;
 
-        GameManager {
-            m_board: board,
-            m_turn: TURN_BLACK,
-            m_num_white: 2,
-            m_num_black: 2,
-            m_next_available_moves: Vec::new(),
+        GameBoard { m_board: board }
+    }
+
+    fn at(&self, position: &Position) -> char {
+        self.m_board[position.m_row as usize][position.m_col as usize]
+    }
+
+    fn is_valid_direction(
+        &self,
+        curr_pos: &Position,
+        row_delta: i8,
+        col_delta: i8,
+        color_self: char,
+        color_opponent: char,
+    ) -> bool {
+        let mut next_pos = Position::new(curr_pos.m_row + row_delta, curr_pos.m_col + col_delta);
+        if next_pos.out_of_bounds() || color_opponent != self.at(&next_pos) {
+            return false;
+        }
+        loop {
+            next_pos.m_row += row_delta;
+            next_pos.m_col += col_delta;
+            if next_pos.out_of_bounds() {
+                return false;
+            }
+            let curr_piece = self.at(&next_pos);
+            if color_self == curr_piece {
+                return true;
+            }
+            if curr_piece == PIECE_EMPTY {
+                return false;
+            }
         }
     }
 
-    pub fn print_board(&self) {
-        println!("  abcedfgh");
+    fn is_available_move(&self, pos: &Position, color_self: char, color_opponent: char) -> bool {
+        if PIECE_EMPTY != self.at(pos) {
+            return false;
+        }
+
+        for delta in &DELTAS {
+            let row_delta = delta[0];
+            let col_delta = delta[1];
+            if self.is_valid_direction(pos, col_delta, row_delta, color_self, color_opponent) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn print(&self) {
+        print!("  ");
+        for &c in &BOARD_LABEL {
+            print!("{}", c);
+        }
+        print!("\n");
+
         let mut row_idx: usize = 0;
         for row in &self.m_board {
             print!("{} ", BOARD_LABEL[row_idx]);
@@ -50,4 +125,193 @@ impl GameManager {
             row_idx += 1;
         }
     }
+
+    fn print_with_next_available_moves(&self, next_available_moves: &BoardLUT) {
+        print!("  ");
+        for &c in &BOARD_LABEL {
+            print!("{}", c);
+        }
+        print!("\n");
+
+        for row in 0..BOARD_DIMENSION {
+            print!("{} ", BOARD_LABEL[row]);
+            for col in 0..BOARD_DIMENSION {
+                let pos = Position::new(row as i8, col as i8);
+                if next_available_moves.contains(&pos) {
+                    print!("*");
+                } else {
+                    print!("{}", self.m_board[row][col]);
+                }
+            }
+            print!("\n");
+        }
+    }
+
+    fn make_move(&mut self, pos: &Position, color_self: char, color_opponent: char) -> usize {
+        let mut num_pieces_flipped: usize = 0;
+        for delta in &DELTAS {
+            let row_delta = delta[0];
+            let col_delta = delta[1];
+            if self.is_valid_direction(pos, row_delta, col_delta, color_self, color_opponent) {
+                let mut next_pos = Position::new(pos.m_row + row_delta, pos.m_col + col_delta);
+                while color_self != self.at(&next_pos) {
+                    self.m_board[next_pos.m_row as usize][next_pos.m_col as usize] = color_self;
+                    num_pieces_flipped += 1;
+                    next_pos.m_row += row_delta;
+                    next_pos.m_col += col_delta;
+                }
+            }
+        }
+
+        self.m_board[pos.m_row as usize][pos.m_col as usize] = color_self;
+        num_pieces_flipped
+    }
+}
+
+struct BoardLUT {
+    m_lut: [[bool; BOARD_DIMENSION]; BOARD_DIMENSION],
+    m_num_set: usize,
+}
+
+impl BoardLUT {
+    fn new() -> BoardLUT {
+        BoardLUT {
+            m_lut: [[false; BOARD_DIMENSION]; BOARD_DIMENSION],
+            m_num_set: 0
+        }
+    }
+
+    fn insert(&mut self, pos: &Position) {
+        self.m_lut[pos.m_row as usize][pos.m_col as usize] = true;
+        self.m_num_set += 1;
+    }
+
+    fn contains(&self, pos: &Position) -> bool {
+        self.m_lut[pos.m_row as usize][pos.m_col as usize]
+    }
+
+    fn is_empty(&self) -> bool {
+        0 == self.m_num_set
+    }
+}
+
+pub struct GameManager {
+    m_board: GameBoard,
+    m_turn: bool,
+    m_game_over: bool,
+    m_num_white: usize,
+    m_num_black: usize,
+    m_next_available_moves: BoardLUT,
+}
+
+impl GameManager {
+    // Factory function
+    pub fn new() -> GameManager {
+        const INITIAL_TURN: bool = TURN_BLACK;
+        let game_board = GameBoard::new();
+        let next_available_moves = generate_next_available_moves(&game_board, INITIAL_TURN);
+
+        GameManager {
+            m_board: game_board,
+            m_turn: INITIAL_TURN,
+            m_game_over: false,
+            m_num_white: 2,
+            m_num_black: 2,
+            m_next_available_moves: next_available_moves,
+        }
+    }
+
+    pub fn print_board(&self, print_available_moves: bool) {
+        if print_available_moves {
+            self.m_board
+                .print_with_next_available_moves(&self.m_next_available_moves);
+        } else {
+            self.m_board.print();
+        }
+    }
+
+    pub fn make_move(&mut self, pos: &Position) -> bool {
+        if false == self.m_next_available_moves.contains(pos) {
+            return false;
+        }
+
+        let color_self = if TURN_WHITE == self.m_turn {
+            PIECE_WHITE
+        } else {
+            PIECE_BLACK
+        };
+        let color_opponent = if PIECE_WHITE == color_self {
+            PIECE_BLACK
+        } else {
+            PIECE_WHITE
+        };
+
+        let num_piece_flipped = self.m_board.make_move(pos, color_self, color_opponent);
+        if 0 == num_piece_flipped {
+            return false;
+        }
+        self.update_count(num_piece_flipped);
+
+        true
+    }
+
+    pub fn check_game_over(&mut self) -> bool {
+        if self.m_game_over {
+            return true;
+        }
+
+        self.switch_turn();
+        self.m_next_available_moves = generate_next_available_moves(&self.m_board, self.m_turn);
+        if false == self.m_next_available_moves.is_empty() {
+            return false;
+        }
+
+        self.switch_turn();
+        self.m_next_available_moves = generate_next_available_moves(&self.m_board, self.m_turn);
+        if false == self.m_next_available_moves.is_empty() {
+            return false;
+        }
+
+        self.m_game_over = true;
+        true
+    }
+
+    fn switch_turn(&mut self) {
+        self.m_turn = !self.m_turn;
+    }
+
+    fn update_count(&mut self, num_piece_flipped: usize) {
+        if TURN_WHITE == self.m_turn {
+            self.m_num_white += num_piece_flipped + 1;
+            self.m_num_black -= num_piece_flipped;
+        } else {
+            self.m_num_white -= num_piece_flipped;
+            self.m_num_black += num_piece_flipped + 1;
+        }
+    }
+}
+
+fn generate_next_available_moves(game_board: &GameBoard, turn: bool) -> BoardLUT {
+    let mut next_available_moves = BoardLUT::new();
+    let color_self = if TURN_WHITE == turn {
+        PIECE_WHITE
+    } else {
+        PIECE_BLACK
+    };
+    let color_opponent = if PIECE_WHITE == color_self {
+        PIECE_BLACK
+    } else {
+        PIECE_WHITE
+    };
+
+    for row in 0..BOARD_DIMENSION {
+        for col in 0..BOARD_DIMENSION {
+            let pos = Position::new(row as i8, col as i8);
+            if false == game_board.is_available_move(&pos, color_self, color_opponent) {
+                continue;
+            }
+            next_available_moves.insert(&pos);
+        }
+    }
+    return next_available_moves;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod game_manager;
 
 use game_manager::GameManager;
+use game_manager::Position;
 
 fn main() {
     let mut game_manager = GameManager::new();
-    game_manager.print_board();
+    game_manager.print_board(true);
+    game_manager.make_move(&Position::new(3, 2));
+    if (game_manager.check_game_over()) {
+        return;
+    }
+    game_manager.print_board(true);
 }
